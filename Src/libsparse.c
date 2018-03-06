2e67a79582c4b9907efc29b60d6a82d77a8b4f27
Nicholas Morrow <nmorrow@crystaldiagnostics.com>
2018-03-05 19:01:26 -0700

    Assign a->cmd outside of argument number check.

--- Src/libsparse.c
+++ Src/libsparse.c
@@ -92,10 +92,10 @@ sparse_StatusTypeDef sparse_Exec(sparse_ParserTypeDef *parser, char *cmd_str)
     }
     /* If this command accepts arguments, build an argpack and pass to the function
        call.*/
+    a->cmd = cmd_str[0];
     unsigned int args = parser->cmd_table[(uint8_t)(cmd_str[0] - 33)].num_args;
     if (args > 0)
     {
-        a->cmd = cmd_str[0];
 
         a->arg_list = (char **)calloc(1, sizeof(char *));
         /* Parse first argument and add to argpack */
